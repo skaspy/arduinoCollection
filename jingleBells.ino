c589844473ad7751fad7f8528f98c4c97c46f715
Sabine Kasparek <38936232+skaspy@users.noreply.github.com>
2021-06-04 19:08:36 +0200

    Add files via upload

---
+++ jingleBells.ino
@@ -0,0 +1,69 @@
+void loop () {
+
+	digitalwrite(9, LOW);  //jin
+	digitalwrite(10, HIGH);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(250);
+
+	digitalwrite(9, LOW); //gle
+	digitalwrite(10, HIGH);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(250);
+
+	digitalwrite(9, LOW); //bells
+	digitalwrite(10, HIGH);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(500);
+
+	digitalwrite(9, LOW); //jin
+	digitalwrite(10, HIGH);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(250);
+
+	digitalwrite(9, LOW); //gle
+	digitalwrite(10, HIGH);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(250);
+
+	digitalwrite(9, LOW); //bells
+	digitalwrite(10, HIGH);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(500);
+
+	digitalwrite(9, LOW); //jin
+	digitalwrite(10, HIGH);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(250);
+
+	digitalwrite(9, HIGH); //gle
+	digitalwrite(10, LOW);
+	digitalwrite(11, LOW);
+	digitalwrite(12, LOW);
+		delay(250);
+
+	digitalwrite(9, LOW); //all
+	digitalwrite(10, LOW); 
+	digitalwrite(11, LOW); 
+	digitalwrite(12, HIGH); 
+		delay(250);
+
+	digitalwrite(9, LOW); //the
+	digitalwrite(10, LOW); 
+	digitalwrite(11, HIGH); 
+	digitalwrite(12, LOW); 
+		delay(250);
+
+	digitalwrite(9, LOW); //way
+	digitalwrite(10, HIGH); 
+	digitalwrite(11, LOW); 
+	digitalwrite(12, LOW); 
+		delay(500);
+
+}
